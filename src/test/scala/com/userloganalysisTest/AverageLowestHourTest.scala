a8959c35284133e6a6899b674d0e235ddfb6424f
cheluvesha <cheluvesh.b@gmail.com>
2020-12-11 01:17:14 +0530

    [CHELUVESHA]Added: split dataframe test case

--- src/test/scala/com/userloganalysisTest/AverageLowestHourTest.scala
+++ src/test/scala/com/userloganalysisTest/AverageLowestHourTest.scala
@@ -1,23 +1,31 @@
 package com.userloganalysisTest
 
 import java.sql.Timestamp
+
 import com.userloganalysis.{AverageLowestHour, UtilityClass}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, to_timestamp}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 class AverageLowestHourTest extends FunSuite with BeforeAndAfterAll {
-  var sparkSession: SparkSession = _
+  var spark: SparkSession = _
   var averageLowestHour: AverageLowestHour = _
+  val data = Seq(("2019-05-21 06:05:02", "xyzname", 10.0, 41.00))
   val csvFilePath = "./Data/CpuLogData2019-09-16.csv"
   val wrongCSV = "./Data/testCSV.csv"
-
+  val column = Seq("datetime", "username", "keyboard", "mouse")
+  var testDF: DataFrame = _
+  val resultDFContent: Array[Row] = Array(
+    Row("xyzname", 10.0, 41.00, "2019-05-21", "06:05:02")
+  )
   override def beforeAll(): Unit = {
-    sparkSession =
-      UtilityClass.createSparkSessionObj("Average lowest hour Test App")
-    averageLowestHour = new AverageLowestHour(sparkSession)
-
+    spark = UtilityClass.createSparkSessionObj("Average lowest hour Test App")
+    averageLowestHour = new AverageLowestHour(spark)
+    val spark1 = spark
+    import spark1.implicits._
+    testDF = data.toDF(column: _*)
+    testDF.withColumn("datetime", to_timestamp(col("datetime")))
   }
-
   test("DataFrameCountMustBeGreaterThanZero") {
     val userlogRowDF = averageLowestHour.readDataFromMySqlForDataFrame()
     assert(userlogRowDF.count() > 0)
@@ -38,7 +46,6 @@ class AverageLowestHourTest extends FunSuite with BeforeAndAfterAll {
   }
   test("checkTheDataOfDataFrame") {
     val rowData = averageLowestHour.readDataFromMySqlForDataFrame().take(1)
-    rowData.foreach(a => println(a))
     var name: Any = null
     var keyboard: Any = null
     var mouse: Any = null
@@ -51,8 +58,14 @@ class AverageLowestHourTest extends FunSuite with BeforeAndAfterAll {
     assert(mouse === 0.0)
     assert(name === "rahilstar11@gmail.com")
   }
+  test(
+    "givenDataFrameAsInputMustPerformSplitTimeAndDateShouldEqualToExceptedDataFrame"
+  ) {
+    val userlogDF = averageLowestHour.splitAndCreateUserlogDF(testDF)
 
+    assert(resultDFContent === userlogDF.collect())
+  }
   override def afterAll(): Unit = {
-    sparkSession.stop()
+    spark.stop()
   }
 }

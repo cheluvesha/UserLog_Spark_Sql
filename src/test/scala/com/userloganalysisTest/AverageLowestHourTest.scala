7bc54eb3f97747de0b42d1be0195e036b8e22f67
cheluvesha <cheluvesh.b@gmail.com>
2020-12-10 19:54:36 +0530

    [CHELUVESHA]Added: DataFrame validation

--- src/test/scala/com/userloganalysisTest/AverageLowestHourTest.scala
+++ src/test/scala/com/userloganalysisTest/AverageLowestHourTest.scala
@@ -1,35 +1,58 @@
 package com.userloganalysisTest
 
+import java.sql.Timestamp
 import com.userloganalysis.{AverageLowestHour, UtilityClass}
 import org.apache.spark.sql.SparkSession
-import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
-class AverageLowestHourTest extends FunSuite with BeforeAndAfter {
-  var spark: SparkSession = _
-  var avgLowHourObj: AverageLowestHour = _
+class AverageLowestHourTest extends FunSuite with BeforeAndAfterAll {
+  var sparkSession: SparkSession = _
+  var averageLowestHour: AverageLowestHour = _
   val csvFilePath = "./Data/CpuLogData2019-09-16.csv"
   val wrongCSV = "./Data/testCSV.csv"
-  before {
-    spark = UtilityClass.createSparkSessionObj("Average lowest hour Test App")
-    avgLowHourObj = new AverageLowestHour
-  }
-  test(
-    "DeclaredDataFrameCreationFromMySQLMustReadAndReturnDataFrameAndItHasToEqualToActual"
-  ) {
-    val userlogDF = avgLowHourObj.readDataFromMySqlForDataFrame()
-    val testDF = spark.read
-      .csv(csvFilePath)
-    val result = DataFrameComparison.compareDataFrame(userlogDF, testDF)
-    assert(result === true)
+
+  override def beforeAll(): Unit = {
+    sparkSession =
+      UtilityClass.createSparkSessionObj("Average lowest hour Test App")
+    averageLowestHour = new AverageLowestHour
+
   }
 
-  test(
-    "DeclaredDataFrameCreationFromMySQLMustReadAndReturnDataFrameAndItHasToNotEqualToActual"
-  ) {
-    val userlogDF = avgLowHourObj.readDataFromMySqlForDataFrame()
-    val testDF = spark.read
-      .csv(wrongCSV)
-    val result = DataFrameComparison.compareDataFrame(userlogDF, testDF)
-    assert(result === false)
+  test("DataFrameCountMustBeGreaterThanZero") {
+    val userlogRowDF = averageLowestHour.readDataFromMySqlForDataFrame()
+    assert(userlogRowDF.count() > 0)
+  }
+
+  test("DateFrameTypeMustToTheGivenType") {
+    val rowList = averageLowestHour.readDataFromMySqlForDataFrame().take(1)
+    rowList.foreach { row =>
+      assert(
+        row.get(0).isInstanceOf[Timestamp],
+        "zero column should be Timestamp"
+      )
+      assert(
+        row.get(1).isInstanceOf[String],
+        "First column should be String"
+      )
+    }
+  }
+  test("checkTheDataOfDataFrame") {
+    val rowData = averageLowestHour.readDataFromMySqlForDataFrame().take(1)
+    rowData.foreach(a => println(a))
+    var name: Any = null
+    var keyboard: Any = null
+    var mouse: Any = null
+    rowData.foreach { row =>
+      name = row.get(1)
+      keyboard = row.get(2)
+      mouse = row.get(3)
+    }
+    assert(keyboard === 0.0)
+    assert(mouse === 0.0)
+    assert(name === "rahilstar11@gmail.com")
+  }
+
+  override def afterAll(): Unit = {
+    sparkSession.stop()
   }
 }

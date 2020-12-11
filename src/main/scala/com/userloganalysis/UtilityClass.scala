811c74d06f5c8d4be28d46b6165bf51270143bbf
cheluvesha <cheluvesh.b@gmail.com>
2020-12-11 22:43:44 +0530

    [CHELUVESHA]Added: Comments

--- src/main/scala/com/userloganalysis/UtilityClass.scala
+++ src/main/scala/com/userloganalysis/UtilityClass.scala
@@ -1,8 +1,16 @@
 package com.userloganalysis
 import org.apache.spark.sql.SparkSession
 
+/***
+  * Utility class which creates Utility functions
+  */
 object UtilityClass {
 
+  /***
+    * Creates SparkSession Object
+    * @param appName String
+    * @return SparkSession
+    */
   def createSparkSessionObj(appName: String): SparkSession = {
     val spark = SparkSession
       .builder()

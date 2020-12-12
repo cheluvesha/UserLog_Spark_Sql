a69683eea7f212f772d3a5991dbdd4487caac2be
cheluvesha <cheluvesh.b@gmail.com>
2020-12-13 02:50:54 +0530

    [CHELUVESHA]Added: Driver class

---
+++ src/main/scala/com/idelhours/IdleHoursDriver.scala
@@ -0,0 +1,71 @@
+package com.idelhours
+
+import com.Utility.{UtilityClass, WriteDataToSource}
+
+object IdleHoursDriver extends App {
+  try {
+
+    val dbName = System.getenv("DB_NAME")
+    val readTable = System.getenv("TABLE")
+    val username = System.getenv("MYSQL_UN")
+    val password = System.getenv("MYSQL_PW")
+    val tableName = "userlog_idlehours"
+    val xmlFilePath = "./idleHrs/UserIdleHours.xml"
+    val jsonFilePath = "./idleHrs/UserIdleHours.json"
+    val sparkSession = UtilityClass.createSparkSessionObj("IdleHours")
+    val idleHours = new IdleHours(sparkSession)
+    val userlogsDF =
+      idleHours.readDataFromMySqlForDataFrame(
+        dbName,
+        readTable,
+        username,
+        password
+      )
+    userlogsDF.printSchema()
+    userlogsDF.show()
+    val keyMouseAnalyzedDF =
+      idleHours.analyzeKeyboardAndMouseDataFromTable(userlogsDF)
+    keyMouseAnalyzedDF.printSchema()
+    keyMouseAnalyzedDF.show()
+    val groupConcatDF = idleHours.groupConcatTheData(keyMouseAnalyzedDF)
+    groupConcatDF.printSchema()
+    groupConcatDF.show()
+    val usersIdleHoursData = idleHours.findIdleHour(groupConcatDF)
+    val usersIdleHoursDF = idleHours.createDataFrame(usersIdleHoursData)
+    usersIdleHoursDF.show()
+    val highToLowIdleHrDF = idleHours.findHighestIdleHour(usersIdleHoursDF)
+    highToLowIdleHrDF.show()
+    val mySqlStatus = WriteDataToSource.writeDataFrameToMysql(
+      highToLowIdleHrDF,
+      dbName,
+      tableName
+    )
+    if (mySqlStatus) {
+      println("Successfully Data written into Mysql table")
+    } else {
+      println("Unable to write Data into Mysql table")
+    }
+    val xmlStatus =
+      WriteDataToSource.writeDataFrameInToXMLFormat(
+        highToLowIdleHrDF,
+        xmlFilePath
+      )
+    if (xmlStatus) {
+      println("Successfully Data written into Xml file")
+    } else {
+      println("Unable to write Data into Xml file")
+    }
+    val jsonStatus = WriteDataToSource.writeDataFrameIntoJsonFormat(
+      highToLowIdleHrDF,
+      jsonFilePath
+    )
+    if (jsonStatus) {
+      println("Successfully Data written into json format")
+    } else {
+      println("Unable to write Data into json format")
+    }
+  } catch {
+    case ex: Exception =>
+      println(ex.printStackTrace())
+  }
+}

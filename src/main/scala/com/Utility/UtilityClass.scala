7c8ee97deda5a0649089000c8cb36110574bf656
cheluvesha <cheluvesh.b@gmail.com>
2020-12-12 18:19:00 +0530

    [CHELUVESHA]Refactored: Separate package for UtilityClass

---
+++ src/main/scala/com/Utility/UtilityClass.scala
@@ -0,0 +1,23 @@
+package com.Utility
+
+import org.apache.spark.sql.SparkSession
+
+/***
+  * Utility class which creates Utility functions
+  */
+object UtilityClass {
+
+  /***
+    * Creates SparkSession Object
+    * @param appName String
+    * @return SparkSession
+    */
+  def createSparkSessionObj(appName: String): SparkSession = {
+    val spark = SparkSession
+      .builder()
+      .appName(appName)
+      .master("local[*]")
+      .getOrCreate()
+    spark
+  }
+}

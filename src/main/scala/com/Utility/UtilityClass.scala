37f2301b3eeb8b028deb70f800731728f2483867
cheluvesha <cheluvesh.b@gmail.com>
2020-12-14 01:03:13 +0530

    [CHELUVESHA]Refactored: Configured local mode

--- src/main/scala/com/Utility/UtilityClass.scala
+++ src/main/scala/com/Utility/UtilityClass.scala
@@ -1,5 +1,6 @@
 package com.Utility
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 /***
@@ -13,9 +14,11 @@ object UtilityClass {
     * @return SparkSession
     */
   def createSparkSessionObj(appName: String): SparkSession = {
+
     val spark = SparkSession
       .builder()
       .appName(appName)
+      .config("spark.driver.host", "localhost")
       .master("local[*]")
       .getOrCreate()
     spark

daee292b658e736152a00d32055507eb76ec1991
cheluvesha <cheluvesh.b@gmail.com>
2020-12-14 01:44:01 +0530

    [CHELUVESHA]Added: Driver class

--- src/main/scala/com/lowestAverageHours/LowestAverageHourDriver.scala
+++ src/main/scala/com/lowestAverageHours/LowestAverageHourDriver.scala
@@ -7,23 +7,42 @@ import org.apache.spark.sql.SparkSession
   * Driver Class calls respective functions to perform analysis and computation on userlogs
   */
 object LowestAverageHourDriver extends App {
-
-  val sparkSession: SparkSession =
-    UtilityClass.createSparkSessionObj("AverageLowestHours")
-  val averageLowestHour = new LowestAverageHour(sparkSession)
-  val userlogsDataFrame = averageLowestHour.readDataFromMySqlForDataFrame()
-  userlogsDataFrame.printSchema()
-  userlogsDataFrame.show()
-  val splittedDataFrame =
-    averageLowestHour.splitAndCreateUserlogDF(userlogsDataFrame)
-  splittedDataFrame.printSchema()
-  splittedDataFrame.show()
-  val minTimeDF = averageLowestHour.findMinAndMaxTimeForUsers(splittedDataFrame)
-  minTimeDF.printSchema()
-  minTimeDF.show()
-  val dailyHourDF = averageLowestHour.calculateDailyHour(minTimeDF)
-  dailyHourDF.printSchema()
-  dailyHourDF.show()
-  val sumByUserDF = averageLowestHour.sumDailyHourWRTUser(dailyHourDF)
-  sumByUserDF.show()
+  try {
+    val dbName = System.getenv("DB_NAME")
+    val readTable = System.getenv("TABLE")
+    val username = System.getenv("MYSQL_UN")
+    val password = System.getenv("MYSQL_PW")
+    val url = System.getenv("URL")
+    val tableName = "userlog_AvgLowHours"
+    val xmlFilePath = "./idleHrs/UserAvgLowHours.xml"
+    val jsonFilePath = "./idleHrs/UserAvgLowHours.json"
+    val sparkSession: SparkSession =
+      UtilityClass.createSparkSessionObj("AverageLowestHours")
+    val averageLowestHour = new LowestAverageHour(sparkSession)
+    val userlogsDataFrame = averageLowestHour.readDataFromMySqlForDataFrame(
+      dbName,
+      readTable,
+      username,
+      password,
+      url
+    )
+    userlogsDataFrame.printSchema()
+    userlogsDataFrame.show()
+    val splittedDataFrame =
+      averageLowestHour.splitAndCreateUserlogDF(userlogsDataFrame)
+    splittedDataFrame.printSchema()
+    splittedDataFrame.show()
+    val minTimeDF =
+      averageLowestHour.findMinAndMaxTimeForUsers(splittedDataFrame)
+    minTimeDF.printSchema()
+    minTimeDF.show()
+    val dailyHourDF = averageLowestHour.calculateDailyHour(minTimeDF)
+    dailyHourDF.printSchema()
+    dailyHourDF.show()
+    val sumByUserDF = averageLowestHour.sumDailyHourWRTUser(dailyHourDF)
+    sumByUserDF.show()
+  } catch {
+    case ex: Exception =>
+      println(ex.printStackTrace())
+  }
 }

76c484a8e318375aff3961ca1ae7feca7406f7e2
cheluvesha <cheluvesh.b@gmail.com>
2020-12-14 23:17:24 +0530

    [CHELUVESHA]Added: Write function calls

--- src/main/scala/com/lowestAverageHours/LowestAverageHourDriver.scala
+++ src/main/scala/com/lowestAverageHours/LowestAverageHourDriver.scala
@@ -1,6 +1,6 @@
 package com.lowestAverageHours
 
-import com.Utility.UtilityClass
+import com.Utility.{UtilityClass, WriteDataToSource}
 import org.apache.spark.sql.SparkSession
 
 /***
@@ -13,13 +13,15 @@ object LowestAverageHourDriver extends App {
     val username = System.getenv("MYSQL_UN")
     val password = System.getenv("MYSQL_PW")
     val url = System.getenv("URL")
-    val tableName = "userlog_AvgLowHours"
-    val xmlFilePath = "./idleHrs/UserAvgLowHours.xml"
-    val jsonFilePath = "./idleHrs/UserAvgLowHours.json"
+    val tableName = "userlog_LowestAvgHours"
+    val xmlFilePath = "./LowAVGHrs/UserAvgLowestHours.xml"
+    val jsonFilePath = "./LowAVGHrs/UserAvgLowestHours.json"
+    val idleHoursTable = "userlog_idlehours"
+    val days = 6
     val sparkSession: SparkSession =
       UtilityClass.createSparkSessionObj("AverageLowestHours")
-    val averageLowestHour = new LowestAverageHour(sparkSession)
-    val userlogsDataFrame = averageLowestHour.readDataFromMySqlForDataFrame(
+    val lowestAverageHour = new LowestAverageHour(sparkSession)
+    val userlogsDataFrame = lowestAverageHour.readDataFromMySqlForDataFrame(
       dbName,
       readTable,
       username,
@@ -28,19 +30,69 @@ object LowestAverageHourDriver extends App {
     )
     userlogsDataFrame.printSchema()
     userlogsDataFrame.show()
+    val selectColFromDF =
+      lowestAverageHour.selectRequiredColumn(userlogsDataFrame)
     val splittedDataFrame =
-      averageLowestHour.splitAndCreateUserlogDF(userlogsDataFrame)
+      lowestAverageHour.splitAndCreateUserlogDF(selectColFromDF)
     splittedDataFrame.printSchema()
     splittedDataFrame.show()
     val minTimeDF =
-      averageLowestHour.findMinAndMaxTimeForUsers(splittedDataFrame)
+      lowestAverageHour.findMinAndMaxTimeForUsers(splittedDataFrame)
     minTimeDF.printSchema()
     minTimeDF.show()
-    val dailyHourDF = averageLowestHour.calculateDailyHour(minTimeDF)
+    val dailyHourDF = lowestAverageHour.calculateDailyHour(minTimeDF)
     dailyHourDF.printSchema()
     dailyHourDF.show()
-    val sumByUserDF = averageLowestHour.sumDailyHourWRTUser(dailyHourDF)
+    val sumByUserDF = lowestAverageHour.sumDailyHourWRTUser(dailyHourDF)
     sumByUserDF.show()
+    val idleHoursDF = lowestAverageHour.readDataFromMySqlForDataFrame(
+      dbName,
+      idleHoursTable,
+      username,
+      password,
+      url
+    )
+    val idleHoursBV = lowestAverageHour.createIdleHoursBroadCast(idleHoursDF)
+    val overAllAverageHourDF =
+      lowestAverageHour.findHourByDifferencingIdleHours(
+        sumByUserDF,
+        idleHoursBV,
+        days
+      )
+    overAllAverageHourDF.show()
+    val lowestAverageHourDF =
+      lowestAverageHour.sortByLowestAverageHours(overAllAverageHourDF)
+    lowestAverageHourDF.show()
+    val mySqlStatus = WriteDataToSource.writeDataFrameToMysql(
+      lowestAverageHourDF,
+      dbName,
+      tableName
+    )
+    if (mySqlStatus) {
+      println("Successfully Data written into Mysql table")
+    } else {
+      println("Unable to write Data into Mysql table")
+    }
+    val xmlStatus =
+      WriteDataToSource.writeDataFrameInToXMLFormat(
+        lowestAverageHourDF,
+        xmlFilePath
+      )
+    if (xmlStatus) {
+      println("Successfully Data written into Xml file")
+    } else {
+      println("Unable to write Data into Xml file")
+    }
+    val jsonStatus = WriteDataToSource.writeDataFrameIntoJsonFormat(
+      lowestAverageHourDF,
+      jsonFilePath
+    )
+    if (jsonStatus) {
+      println("Successfully Data written into json format")
+    } else {
+      println("Unable to write Data into json format")
+    }
+    sparkSession.stop()
   } catch {
     case ex: Exception =>
       println(ex.printStackTrace())

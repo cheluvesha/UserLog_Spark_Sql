e6c1b92a3d1df9fa6ed0278ae3027c9c988c7cd9
cheluvesha <cheluvesh.b@gmail.com>
2020-12-15 01:44:48 +0530

    [CHELUVESHA]Added: Test case Exception for readDataFromHDFS

--- src/test/scala/com/userloganalysisTest/UserlogLateAnalysisTest.scala
+++ src/test/scala/com/userloganalysisTest/UserlogLateAnalysisTest.scala
@@ -12,6 +12,7 @@ class UserlogLateAnalysisTest extends FunSuite with BeforeAndAfterAll {
   var userlogLateAnalysis: UserlogLateAnalysis = _
   val hdfsFilePath =
     "hdfs://localhost:54310//userlogs_header/CpuLogData2019-09-16.csv"
+  val wrongHDFSPath = "hdfs://localhost:54310//urlogs_header"
   override def beforeAll(): Unit = {
     spark = UtilityClass.createSparkSessionObj("Late Analysis Test")
     userlogLateAnalysis = new UserlogLateAnalysis(spark)
@@ -35,6 +36,12 @@ class UserlogLateAnalysisTest extends FunSuite with BeforeAndAfterAll {
           "Forty column should be String"
         )
       })
+  }
+  test("givenWrongHDFSFilePathMustTriggerAnException") {
+    val thrown = intercept[Exception] {
+      userlogLateAnalysis.readFilesFromHDFS(wrongHDFSPath)
+    }
+    assert(thrown.getMessage === "Please Check The Path Specified")
   }
 
 }

3001c4d7f88aadae2ce10335230c869badc90f7f
cheluvesha <cheluvesh.b@gmail.com>
2020-12-15 14:32:35 +0530

    [CHELUVESHA]Added: Select and findlogin time methods

--- src/main/scala/com/highestNoOfTimeLateComing/UserlogLateAnalysis.scala
+++ src/main/scala/com/highestNoOfTimeLateComing/UserlogLateAnalysis.scala
@@ -1,5 +1,6 @@
 package com.highestNoOfTimeLateComing
 
+import org.apache.spark.sql.functions.{col, min, to_date}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /***
@@ -14,10 +15,56 @@ class UserlogLateAnalysis(sparkSession: SparkSession) {
     * @return DataFrame
     */
   def readFilesFromHDFS(hdfsFilePath: String): DataFrame = {
-    val userlogsDF = sparkSession.read
-      .option("inferSchema", value = true)
-      .option("header", value = true)
-      .csv(hdfsFilePath)
-    userlogsDF
+    try {
+      val userlogsDF = sparkSession.read
+        .option("inferSchema", value = true)
+        .option("header", value = true)
+        .csv(hdfsFilePath)
+      userlogsDF
+    } catch {
+      case sqlAnalysisException: org.apache.spark.sql.AnalysisException =>
+        throw new Exception("Please Check The Path Specified")
+    }
   }
+
+  /***
+    * Selects the required rows from the DataFrame
+    * @param userlogsDF DataFrame
+    * @return DataFrame
+    */
+  def selectRequiredColumn(userlogsDF: DataFrame): DataFrame = {
+    try {
+      val selectedDF = userlogsDF.select(
+        col("Datetime"),
+        to_date(col("Datetime"), "yyyy-MM-dd") as "dates",
+        col("user_name")
+      )
+      selectedDF
+    } catch {
+      case sqlException: org.apache.spark.sql.AnalysisException =>
+        throw new Exception("SQL Syntax Error Please Check The Syntax")
+      case ex: Exception =>
+        throw new Exception("Unable to split and create userlog dataframe")
+    }
+  }
+
+  /***
+    * finds minimum time i.e Login time through Group by and creates loginTimeDF DataFrame
+    * @param userLogsDataFrame DataFrame
+    * @return DataFrame
+    */
+  def findLoginTimeForUsers(userLogsDataFrame: DataFrame): DataFrame = {
+    try {
+      val loginTimeDF = userLogsDataFrame
+        .groupBy("user_name", "dates")
+        .agg(min("Datetime") as "login_time")
+      loginTimeDF
+    } catch {
+      case sqlException: org.apache.spark.sql.AnalysisException =>
+        throw new Exception("SQL Syntax Error Please Check The Syntax")
+      case ex: Exception =>
+        throw new Exception("Unable to find login time to create DataFrame")
+    }
+  }
+
 }

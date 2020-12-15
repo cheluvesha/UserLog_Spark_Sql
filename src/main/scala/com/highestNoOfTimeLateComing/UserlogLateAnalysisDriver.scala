d3fbdf5356e59e1bc34c31048790112616eccf36
cheluvesha <cheluvesh.b@gmail.com>
2020-12-16 00:38:57 +0530

    [CHELUVESHA]Refactored: Separate WriteDataTo.. various sources methods

--- src/main/scala/com/highestNoOfTimeLateComing/UserlogLateAnalysisDriver.scala
+++ src/main/scala/com/highestNoOfTimeLateComing/UserlogLateAnalysisDriver.scala
@@ -9,20 +9,33 @@ import org.apache.spark.sql.DataFrame
 object UserlogLateAnalysisDriver extends App {
   val hdfsFilePath = System.getenv("hdfsFilePath")
   val dbName = System.getenv("DB_NAME")
-  val tableName = "userlog_lateComers"
-  val xmlFilePath = "./late/lateComers.xml"
-  val jsonFilePath = "./late/lateComers.json"
+  val lateComerTable = "userlog_lateComers"
+  val noOfLeavesTable = "userlog_noOfLeaves"
+  val averageLateHoursTable = "userlog_averageLateHours"
+  val lateComerXmlFilePath = "./late/lateComers.xml"
+  val lateComerJsonFilePath = "./late/lateComers.json"
+  val noOfLeavesXmlFilePath = "./leaves/noOfLeaves.xml"
+  val noOfLeavesJsonFilePath = "./leaves/noOfLeaves.json"
+  val averageLateHoursXml = "./averageLateHours/lateHour.xml"
+  val averageLateHoursJson = "./averageLateHours/lateHour.json"
   val sparkSession = UtilityClass.createSparkSessionObj("UserlogLateAnalysis")
   val loginTime = "09:00:00"
   val userlogLateAnalysis = new UserlogLateAnalysis(sparkSession)
+  var selectedUserlogDF: DataFrame = _
   var appendLoginTimeDF: DataFrame = _
   var noOfTimesLateComingDF: DataFrame = _
   var highestNoOfLateComersDF: DataFrame = _
+  var totalNumberOfLeavesDF: DataFrame = _
+  var averageLateHoursDF: DataFrame = _
+
+  /***
+    * Calls the respective functions to find the number of late coming users
+    */
   def findNoOfLateComing(): Unit = {
     val userlogsDF = userlogLateAnalysis.readFilesFromHDFS(hdfsFilePath)
     userlogsDF.show()
     userlogsDF.printSchema()
-    val selectedUserlogDF = userlogLateAnalysis.selectRequiredColumn(userlogsDF)
+    selectedUserlogDF = userlogLateAnalysis.selectRequiredColumn(userlogsDF)
     selectedUserlogDF.printSchema()
     selectedUserlogDF.show()
     val loginTimeDF =
@@ -42,6 +55,10 @@ object UserlogLateAnalysisDriver extends App {
     highestNoOfLateComersDF.printSchema()
     highestNoOfLateComersDF.show(false)
   }
+
+  /***
+    * Calls the respective functions to find the average late coming users
+    */
   def findAverageLateHours(): Unit = {
     val usersLateHourDF =
       userlogLateAnalysis.findUsersLateHours(appendLoginTimeDF)
@@ -53,17 +70,40 @@ object UserlogLateAnalysisDriver extends App {
     totalLateHoursDF.printSchema()
     val broadcastNoOfTimesLate =
       userlogLateAnalysis.broadcastNoOfTimesLateComing(noOfTimesLateComingDF)
-    val averageLateHoursDF =
-      userlogLateAnalysis.findAverageLateHours(
-        totalLateHoursDF,
-        broadcastNoOfTimesLate
-      )
+    averageLateHoursDF = userlogLateAnalysis.findAverageLateHours(
+      totalLateHoursDF,
+      broadcastNoOfTimesLate
+    )
     averageLateHoursDF.printSchema()
     averageLateHoursDF.show()
   }
-  def writeHighestNoOfLateComingDataToSources(): Unit = {
+
+  /***
+    * Calls the respective functions to find the total number of leaves taken by users
+    */
+  def findTotalNumberOfLeaves(): Unit = {
+    val noOfWorkingDays =
+      userlogLateAnalysis.findNoOfWorkingDays(selectedUserlogDF)
+    totalNumberOfLeavesDF = userlogLateAnalysis.findUsersTotalNumberOfLeaves(
+      noOfWorkingDays,
+      selectedUserlogDF
+    )
+    totalNumberOfLeavesDF.show()
+  }
+
+  /***
+    * Writes the DataFrame to Mysql table
+    * @param dataFrame DataFrame
+    * @param dbName String
+    * @param tableName String
+    */
+  def writeDataToMysqlTable(
+      dataFrame: DataFrame,
+      dbName: String,
+      tableName: String
+  ): Unit = {
     val mySqlStatus = WriteDataToSource.writeDataFrameToMysql(
-      highestNoOfLateComersDF,
+      dataFrame,
       dbName,
       tableName
     )
@@ -72,9 +112,41 @@ object UserlogLateAnalysisDriver extends App {
     } else {
       println("Unable to write Data into Mysql table")
     }
+  }
+
+  /***
+    * Writes the DataFrame to Json file
+    * @param analyzedDataFrame DataFrame
+    * @param jsonFilePath String
+    */
+  def writeDataToJsonFile(
+      analyzedDataFrame: DataFrame,
+      jsonFilePath: String
+  ): Unit = {
+    val jsonStatus = WriteDataToSource.writeDataFrameIntoJsonFormat(
+      analyzedDataFrame,
+      jsonFilePath
+    )
+    if (jsonStatus) {
+      println("Successfully Data written into json format")
+    } else {
+
+      println("Unable to write Data into json format")
+    }
+  }
+
+  /***
+    * Writes the DataFrame to XMl file
+    * @param analyzedDataFrame DataFrame
+    * @param xmlFilePath String
+    */
+  def writeDataToXml(
+      analyzedDataFrame: DataFrame,
+      xmlFilePath: String
+  ): Unit = {
     val xmlStatus =
       WriteDataToSource.writeDataFrameInToXMLFormat(
-        highestNoOfLateComersDF,
+        analyzedDataFrame,
         xmlFilePath
       )
     if (xmlStatus) {
@@ -82,14 +154,17 @@ object UserlogLateAnalysisDriver extends App {
     } else {
       println("Unable to write Data into Xml file")
     }
-    val jsonStatus = WriteDataToSource.writeDataFrameIntoJsonFormat(
-      highestNoOfLateComersDF,
-      jsonFilePath
-    )
-    if (jsonStatus) {
-      println("Successfully Data written into json format")
-    } else {
-      println("Unable to write Data into json format")
-    }
   }
+  findNoOfLateComing()
+  findAverageLateHours()
+  findTotalNumberOfLeaves()
+  writeDataToMysqlTable(highestNoOfLateComersDF, dbName, lateComerTable)
+  writeDataToMysqlTable(averageLateHoursDF, dbName, averageLateHoursTable)
+  writeDataToMysqlTable(totalNumberOfLeavesDF, dbName, noOfLeavesTable)
+  writeDataToJsonFile(highestNoOfLateComersDF, lateComerJsonFilePath)
+  writeDataToJsonFile(averageLateHoursDF, averageLateHoursJson)
+  writeDataToJsonFile(totalNumberOfLeavesDF, noOfLeavesJsonFilePath)
+  writeDataToXml(highestNoOfLateComersDF, lateComerXmlFilePath)
+  writeDataToXml(averageLateHoursDF, averageLateHoursXml)
+  writeDataToXml(totalNumberOfLeavesDF, noOfLeavesXmlFilePath)
 }
